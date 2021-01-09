00c9a201b3d6d0b65b589aa25355f79504b408ea
kalev21 <pfn21@bk.ru>
2021-01-09 22:04:07 +0300

    Коммит Переменной let

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use script';
+
+let message;        //Переменная Let
+message = 'Hello!'  // Переменной message присваевается с помощью '=' значение Hello.
+
+let user = 'John', age = 25, message = 'Hello';     //Присваивание переменных в одной строке.
+let user = 'John';                                  //Многострочный вариант присваивания переменных
+let age = 25;
+let message = 'Hello';
