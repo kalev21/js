37fdb10cc0f9285ec7edf3b3e54ceefaa338c9e5
kalev21 <pfn21@bk.ru>
2021-01-16 00:09:46 +0300

    Коммит 3 и 4 задачи

--- js/script.js
+++ js/script.js
@@ -2,12 +2,34 @@
 
 let foreName = 'Fedor';        //Переменная литерал строка
 console.log(foreName);
+//Задача 3
+function square(n) {
+    return Math.pow(n, 2);
+}
+console.log(square(2));  //квадрат аргумента
+
+function cube(a) {
+    return Math.pow(a, 3);
+}
+console.log(cube(2));   //куб аргумента
+
+
+//Задача 4
+const fn = null; {
+    console.log(`Name, ${null.name}`);
+};
+const Fedor = {name: 'Fedor Pyatakov'};
+let Oleg = {name: 'Oleg Nikiforov'};
 
 const dateBirth = 1989;        //Константа литерал число
 console.log(dateBirth);
+fn(Fedor);
+fn(Oleg);
 
 
 function showMessage(name)  {   //Функция вывода имени
     alert(name); 
+function createUser(name, city) {
+    return createUser(name, city);
 }
-showMessage('Фёдор');
+showMessage('Фёдор');console.log("'name: 'Marcus Aurelius', city: 'Roma'"); 
